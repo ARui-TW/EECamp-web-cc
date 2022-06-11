c29137069aac1ad275e0fc096ff25b15fb6b413d
ChangChi <sam54537@gmail.com>
2022-06-12 04:10:03 +0800

    feat: basic chatbot

--- pages/info.vue
+++ pages/info.vue
@@ -133,13 +133,29 @@
         </div>
       </v-col>
     </v-row>
+    <VueBotUI :messages="messages" @msg-send="messageSendHandler" />
   </v-container>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import { VueBotUI } from 'vue-bot-ui';
+import { Amplify } from 'aws-amplify';
+import { Interactions } from 'aws-amplify';
+import awsConfig from '../aws-exports';
+
 export default {
+  components: {
+    VueBotUI
+  },
   data: () => ({
+    messages: [
+      {
+        agent: 'bot',
+        type: 'text',
+        text: 'Hello. How can I help you'
+      }
+    ],
     playerVars: {
       autoplay: 1,
       mute: 1
@@ -157,6 +173,24 @@ export default {
     ...mapGetters({
       campInfo: 'Web/GetCampInfo'
     })
+  },
+  methods: {
+    async messageSendHandler(message) {
+      this.messages.push({
+        agent: 'user',
+        type: 'text',
+        text: message.text
+      });
+      const response = await Interactions.send('EECampBot_dev', message.text);
+      this.messages.push({
+        agent: 'bot',
+        type: 'text',
+        text: response.message
+      });
+    }
+  },
+  mounted() {
+    Amplify.configure(awsConfig);
   }
 };
 </script>
