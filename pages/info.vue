9e8fc93abd9b516d1e7c916771eb864b069cd310
108062213 <xyhy108062213@gapp.nthu.edu.tw>
2022-06-13 01:47:46 +0800

    feat: textract function

--- pages/info.vue
+++ pages/info.vue
@@ -66,6 +66,14 @@
             More Pictures!
           </v-btn>
         </div>
+        
+        <div class="d-flex flex-column flex-md-row justify-space-between">
+          <v-btn click="detect_document_text">
+            <SizeBox width="10" />
+            page test
+          </v-btn>
+        </div>
+
         <SizeBox height="10" />
         <v-sheet width="100%">
           <youtube
@@ -157,6 +165,48 @@ export default {
     ...mapGetters({
       campInfo: 'Web/GetCampInfo'
     })
+  }
+};
+</script>
+
+<script>
+  const AWS = require("aws-sdk")
+
+export default {
+  methods: {
+    async detect_document_text () {
+      const client = new AWS.Textract();
+      const s3 = new AWS.S3();
+      const params = {
+        Document: {
+          S3Object: {
+            Bucket: bucket,
+            Name: photo
+          },
+        },
+      }
+
+      const displayBlockInfo = async (response) => {
+        try {
+            response.Blocks.forEach(block => {
+              console.log(`Block Type: ${block.BlockType}`),
+              console.log(`Text: ${block.Text}`)
+              console.log(`TextType: ${block.TextType}`)
+              console.log(`Confidence: ${block.Confidence}`)
+              console.log("-----")
+            });
+          } catch (err) {
+            console.log("Error", err);
+          }
+        }
+
+      try {
+        const res = await client.detectDocumentText(params).promise();
+        displayBlockInfo(res);
+      } catch (err) {
+        console.log("Error", err);
+      }
+    }
   }
 };
 </script>
